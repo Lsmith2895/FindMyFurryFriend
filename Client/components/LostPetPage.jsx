1d07bd3c9136c42f7d6c904027fbce0135480f1f
Logan Smith <lsmith2895@yahoo.com>
2020-11-02 10:32:39 -0800

    set up and style Lost dog form

--- Client/components/LostPetPage.jsx
+++ Client/components/LostPetPage.jsx
@@ -2,46 +2,57 @@ import React from 'react';
 
 const LostPet = props => (
     <div>
-        <form className='LostPetForm'>
-            <div>Owner Information</div>
+        <form className='LostPetForm' action='' method='post'>
+            <h3>Owner Information</h3>
             <label>
-              Owners Name:
-              <input type="text" name="OwnerName" placeholder="Your Name" />
-            </label>
-            <div>Contact Information</div>
-            <label>
-              Email:
-              <input type="email" name="Email" placeholder="Email@host.com" />
+                <h4>Name</h4>
+                <input type="text" name="OwnerName" placeholder="Your Name" />
             </label>
             <label>
-                Phone Number:
-                <input type="tel" 
-                    name="Phone" 
+                <h4>Email</h4>
+                <input type="email" name="Email" placeholder="Email@host.com" />
+            </label>
+            <label>
+                <h4>Phone Number</h4>
+                <input type="tel"
+                    name="Phone"
                     placeholder="1234567891"
                     pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                     required
-                    maxlength='10'
+                    maxlength='12'
                 />
             </label>
-            <div>Pet Information</div>
+            <h3>Pet Information</h3>
             <label>
-                Pet's Name:
+                <h4>Name</h4>
                 <input type="text" name="PetName" placeholder="Pet Name" />
             </label>
             <label>
-                Collar Color:
+                <h4>Collar Color</h4>
                 <input type="text" name="Collar" placeholder="Color" />
             </label>
             <label>
-                Friendliness:
+                <h4>Size</h4>
+                XS: <input type='radio' name='size' value="XS" />
+                S: <input type='radio' name='size' value="S" />
+                M: <input type='radio' name='size' value="M" checked/>
+                L: <input type='radio' name='size' value="L" />
+                XL: <input type='radio' name='size' value="XL" />
+            </label>
+            <label>
+                <h4>Friendliness Rating</h4>
                 Friendly: <input type='radio' name='Friendliness' value='Friendly' />
-                Nervous:<input type='radio' name='Friendliness' value='Nervous' />
+                Nervous:<input type='radio' name='Friendliness' value='Nervous' checked />
                 Do Not Approach:<input type='radio' name='Friendliness' value='Do Not Approach' />
             </label>
+            <label>
+                <h4>Photo of Pet:</h4>
+                <input type="file" id="input" multiple></input>
+            </label>
             <div>
-            <input type="submit" name="Submit" />
+                <input type="submit" name="Submit" />
             </div>
-            
+
         </form>
 
     </div>
--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const db = require('./db.js')
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -11,23 +13,12 @@ app.get('/', (req, res) => {
 
 //empty request handlers
 app.post('/', (req, res) => {
-  res.send('hello from empty post request')
+  res.send(req)
 })
 
 app.get('/', (req, res) => {
   res.send('hello from empty get request')
 })
-
-app.put('/', (req, res) => {
-  res.send('hello from empty put request')
-})
-app.patch('/', (req, res) => {
-  res.send('hello from empty patch request')
-})
-app.delete('/', (req, res) => {
-  res.send('hello from empty delete request')
-})
-
 
 app.listen(PORT, () => {
   console.log('you have connected find my furry friend')
