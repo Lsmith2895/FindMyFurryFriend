686b5f363f45a7928c3b01594a43e4e5923594a2
Logan Smith <lsmith2895@yahoo.com>
2020-11-02 20:16:55 -0800

    test to move routes into twillio router

--- Server/TwillioRouter.js
+++ Server/TwillioRouter.js
@@ -8,19 +8,37 @@ module.exports = function () {
     var client = new Twilio(config.accountSid, config.authToken);
     const router = Express.Router();
 
-    // Send lead notification
+    app.post('/api/LostForm', (req, res) => {
+        if (err) {
+          console.log('error in app.post: ', err)
+          res.send(500)
+        } else {
+          db.query(
+            `INSERT INTO LostPets VALUES ('${req.body.OwnerName}', '${req.body.Email}', '${req.body.Phone}', '${req.body.PetName}', '${req.body.Collar}', '${req.body.size}', '${req.body.Friendliness}', '${req.body.LastZip}', '${req.body.LastSeen}', '${req.body.Photo}');`
+          )
+          .then(() => {
+            axios({
+              method: 'post',
+              url: '/leads',
+              host: 'localhost:3000'
+            });
+            res.sendStatus(200)
+          })
+        }
+      })
+    // Send lost notification
     router.post('/leads', function(request, response) {
         // Assemble a text message body
         var message = 'A pet has been lost in your area,' +
-        ' please help us get our furry friend ' + petname +
-        ' back there parents. They were last Seen at ' + lastseen + '.' +
-        petname + ' has a' + collar + 'on. If you find this friend' +
+        ' please help us get our furry friend ' + 'petname' +
+        ' back there parents. They were last Seen at ' + 'lastseen' + '.' +
+        'petname' + ' has a' + 'collar' + 'on. If you find this friend' +
         'be sure to go to our website and select the found button' +  
         ' on the Home page beneath the pet\'s bio -Thanks from the Furry Friend Family';
 
-        // Send lead notification to agent
+        // Send lost notification to local
         client.messages.create({
-            to: config.agentNumber, //FIX ME to be customer number
+            to: config.agentNumber,
             from: config.twilioNumber, 
             body: message
         })
--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const db = require('./db.js')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const { default: axios } = require('axios');
+const router = require('');
 
 app.use(bodyParser.json())
 app.use(express.static('public'));
@@ -10,16 +12,9 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
 })
 
+app.use('/', router);
 
-//empty request handlers
-app.post('/api/LostForm', (req, res) => {
-  console.log('Reached Lost Form api call', req.body.OwnerName);
-  db.query(
-    `INSERT INTO LostPets VALUES ('${req.body.OwnerName}', '${req.body.Email}', '${req.body.Phone}', '${req.body.PetName}', '${req.body.Collar}', '${req.body.size}', '${req.body.Friendliness}', '${req.body.LastZip}', '${req.body.LastSeen}', '${req.body.Photo}');`
-  )
-  
-  res.sendStatus(200)
-})
+
 
 app.get('/', (req, res) => {
   res.send('hello from empty get request')
