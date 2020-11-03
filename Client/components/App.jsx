89c34e94f12e8d9c4c7b33d01fc40cc1b5c247cf
Logan Smith <lsmith2895@yahoo.com>
2020-11-03 15:09:25 -0800

    set load page to home (previously set to lost for debugging

--- Client/components/App.jsx
+++ Client/components/App.jsx
@@ -7,7 +7,7 @@ class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            page: 'lost',
+            page: 'home',
             OwnerName: 'john',
             Email: 'klajsd;lfkj',
             Phone: '123-456-7891',
