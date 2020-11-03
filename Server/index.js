b178d50bca4e5386b3a49f98e6cbee73c03c091b
Logan Smith <lsmith2895@yahoo.com>
2020-11-02 22:00:14 -0800

    set up Twilio rout to send messages (commented out messenge route to reduce trial cost)

--- Server/index.js
+++ Server/index.js
@@ -1,24 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const db = require('./db.js')
 const bodyParser = require('body-parser');
-const { default: axios } = require('axios');
-const router = require('');
+const router = require('./TwillioRouter.js')();
 
 app.use(bodyParser.json())
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-})
-
 app.use('/', router);
-
-
-
-app.get('/', (req, res) => {
-  res.send('hello from empty get request')
-})
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
